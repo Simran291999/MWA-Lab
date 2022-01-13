12834c98d165e77dbc70e4c67fc44c996ee4c62f
CryptoLeo-3000 <padavekartik@gmail.com>
2022-01-13 09:56:47 +0530

    PHP Session 1

---
+++ Sessions/PHP/php1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Session 1</title>
+</head>
+<body>
+    <?php
+    $name = "Kartik";
+    $surname = "Padave";
+    $id_no = "A022";
+    $todays_date = date("I F d, Y");
+
+    print("Name is ".$name);
+    print("<br>Surname is ".$surname);
+    print("<br>ID is ".$id_no);
+    print("<br>Date is ".$todays_date);
+    ?>
+</body>
+</html>
