19b4d3e7cb2e58d414dc72ebeec1cb48efbc257b
CryptoLeo-3000 <padavekartik@gmail.com>
2022-01-17 10:24:39 +0530

    Automatic form validation

--- Sessions/Practical/AutoFormValid/form.js
+++ Sessions/Practical/AutoFormValid/form.js
@@ -4,39 +4,37 @@ function Validation() {
     var subject = document.getElementById("6course");
     var password = document.getElementById("3password");
     var confirmpassword = document.getElementById("4confirmpassword");
-
+    var age = document.getElementById("7age");
 
     if (name.value == "") {
-        window.alert("Please enter your name.");
-        name.focus();
+        document.getElementById("name-demo").innerHTML = "Enter Name";
         return false;
     }
 
-    if(!phone.checkValidity()) {
-        document.getElementById("phone-demo").innerHTML = inpobj.validationMessage;
-    }
-
-    if (phone.value == "" || phone.value < 10) {
-        window.alert("Please enter your Phone number.");
-        phone.focus();
+    if(!age.checkValidity()) {
+        document.getElementById("age-demo").innerHTML = "You are not eligible to apply";
         return false;
     }
 
     if (password.value == "") {
-        window.alert("Please enter your password");
+        window.alert("Please enter a password");
         password.focus();
         return false;
     }
 
     if (confirmpassword.value != password.value) {
         window.alert("Password does not match");
-        password.focus();
+        confirmpassword.focus();
+        return false;
+    }
+
+    if(phone.value == "") {
+        document.getElementById("phone-demo").innerHTML = "Enter valid phone number";
         return false;
     }
 
     if (subject.selectedIndex < 1) {
-        alert("Please select a course");
-        subject.focus();
+        document.getElementById("course-demo").innerHTML = "Choosing a course is mandatory";
         return false;
     }
 
