f24216e0e74267022ce249cd7de1ed12ed20838d
CryptoLeo-3000 <padavekartik@gmail.com>
2022-01-24 11:03:39 +0530

    Login & Registeration

--- Sessions/Practical/AutoFormValid/login/login.js
+++ Sessions/Practical/AutoFormValid/login/login.js
@@ -1,12 +1,20 @@
 function Validation() {
-    var user = document.getElementById("Uname");
-    var pass = document.getElementById("Upassword");
+    var user = document.getElementById("Uname").value;
+    var pass = document.getElementById("Upassword").value;
 
-    if (user == null || user == ""){  
-        alert("Username can't be blank");  
-        return false;  
+    if (user == null || user == ""){
+        alert("Username can't be blank");
+        return false;
     } else if (pass == null || pass == ""){
-        alert("Please enter valid password");  
-        return false;  
-    }  
+        alert("Please enter password");
+        return false;
+    } else if (user != "Kartik39") {
+        alert("Wrong username");
+        return false;
+    } else if (pass != "70362019039") {
+        alert("Wrong password");
+        return false;
+    }
+
+    return true;
 }
--- Sessions/Practical/AutoFormValid/login/login.php
+++ Sessions/Practical/AutoFormValid/login/login.php
@@ -1,14 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register Output</title>
-</head>
-<body>
-    <h1>You are valid user</h1>
-    <br>
-    <h4>Thanks for visiting</h4>
-</body>
-</html>
+<?php
+echo "<h1>You are a valid User</h1>";
+echo "<h4>Welcome</h4>";
+echo "Name: Kartik Padave<br>";
+echo "SAP ID: 70362019039<br>";
+echo "Course: B.Tech 3rd year<br>";
+echo "Email ID: padavekartik@gmail.com<br>";
+?>
