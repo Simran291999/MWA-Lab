d9bb328a8a28a2279bd6986112979fde8dc0bb08
CryptoLeo-3000 <padavekartik@gmail.com>
2022-02-08 14:57:18 +0530

    SQL Lectures

--- Sessions/SQL/php2.php
+++ Sessions/SQL/php2.php
@@ -11,12 +11,12 @@ if ($conn->connect_error) {
 }
 
 $sql = "CREATE TABLE MyGuests (
-    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    firstname VARCHAR(30) NOT NULL,
-    lastname VARCHAR(30) NOT NULL,
-    email VARCHAR(50),
-    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-    )";
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  firstname VARCHAR(30) NOT NULL,
+  lastname VARCHAR(30) NOT NULL,
+  email VARCHAR(50),
+  reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
 
 if ($conn->query($sql) === TRUE) {
   echo "Table MyGuests created successfully";
